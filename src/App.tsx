54df89d7f02f78bafa31386eb9bf3d3e875d7df4
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2023-04-21 09:20:31 +0200

    allow arbitrary imagemagic convert commands

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ImageUploader from "react-images-upload";
 import './App.css';
-import { Button } from '@mui/material';
+import { Button, Input } from '@mui/material';
 import { buildInputFile, execute } from 'wasm-imagemagick';
 
 function App() {
+
+  let init_command = "convert %image -compress none out.ppm";
+  const urlParams = new URLSearchParams(window.location.search);
+  const command_param = urlParams.get('command');
+  if (command_param) {
+    init_command = command_param;
+  }
+
+  const [command, setCommand] = React.useState<string>(init_command);
 
   const [picture, setPicture] = React.useState<[File, string] | null>(null)
   const onDrop = (pictureFiles: File[], pictureDataURLs: string[]) => {
@@ -13,17 +22,19 @@ function App() {
   const convert = async () => {
     if (!picture)
       return;
-    const inputFile = await buildInputFile(picture[1], 'input.jpg');
+    const file_name = picture[0].name;
+    console.log(file_name);
+    const inputFile = await buildInputFile(picture[1], file_name);
     const { outputFiles } = await execute({
       inputFiles: [inputFile],
-      commands: 'convert input.jpg -compress none out.ppm',
+      commands: command.replace("%image", file_name),
     });
     const output = outputFiles[0];
     const blob = output.blob;
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = 'out.ppm';
+    a.download = output.name;
     a.click();
   };
 
@@ -36,11 +47,16 @@ function App() {
         onChange={onDrop}
         imgExtension={[".jpg", ".gif", ".png", ".gif", ".webp"]}
         maxFileSize={5242880}
+        singleImage={true}
       />
 
       <Button variant="contained" color="primary" onClick={convert} disabled={!picture}>
         Convert to PPM
       </Button>
+
+      <br />
+
+      <Input type="text" value={command} onChange={(e) => setCommand(e.target.value)} fullWidth />
     </div>
   );
 }
